8e898822710d4b3274a58edaf4b6a965d4daeaaa
Michael Geo Josh M.S <geojosh432002@gmail.com>
2023-07-15 12:57:57 +0530

    small commit made in guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { JwtStrategy } from './auth/jwt.strategy';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule,PassportModule,JwtModule.register({secret:'secrete',signOptions:{ expiresIn:'1h'}})],
+  controllers: [AppController,AuthController],
+  providers: [AppService,AuthService,JwtStrategy],
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,35 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthenticateDto } from './dto/authenticate.dto';
+import { JwtAuthGuard } from './jwt.guard';
+import { Roles } from './roles/roles.decorator';
+import { RoleGuard } from './role/role.guard';
+import { Request,Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
+    @Roles('admin','customer')
+    @UseGuards(JwtAuthGuard,RoleGuard)
+    @Get()
+
+    profile(@Req() req:Request, @Res() res:Response) 
+    {
+        return res.status(HttpStatus.OK).json(req.user);
+    }
+
     @Post()
 
-    login(@Res() res, @Body() AuthenticateDto: AuthenticateDto) {
-        try {
+    login(@Res() res :Response, @Body() AuthenticateDto: AuthenticateDto) 
+    {
+        try 
+        {
             const response = this.authService.authenticate(AuthenticateDto);
             return res.status(HttpStatus.OK).json({ response });
         }
-        catch (error) {
+        catch (error) 
+        {
             return res.status(error.status).json(error.response);
         }
     }
